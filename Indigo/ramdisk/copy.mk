1fbe64195b8d4f34fe4f4b7840d904d224a5642b
Koshu <mail@koshu.de>
2012-09-19 18:02:39 +0200

    added mk files

---
+++ Indigo/ramdisk/copy.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init:system/init \
+    $(LOCAL_PATH)/init.goldfish.rc:system/init.goldfish.rc \
+    $(LOCAL_PATH)/init.indigo.usb.rc:system/init.indigo.usb.rc \
+    $(LOCAL_PATH)/init.nv_dev_board.usb.rc:system/init.nv_dev_board.usb.rc \
+    $(LOCAL_PATH)/init.ventana.rc:system/init.ventana.rc \
+    $(LOCAL_PATH)/ueventd.goldfish.rc:system/ueventd.goldfish.rc \
+    $(LOCAL_PATH)/ueventd.rc:system/ueventd.rc \
+    $(LOCAL_PATH)/ueventd.ventana.rc:system/ueventd.ventana.rc \
